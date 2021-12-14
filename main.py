61edad9138195a0ad9c5b4e18593bac46475cff9
peetpan <peetamber.p@somaiya.edu>
2021-12-14 18:19:52 +0530

    fc

---
+++ main.py
@@ -0,0 +1,88 @@
+import cv2
+import numpy as np 
+
+def load_yolo():
+    # net = cv2.dnn.readNet("yolov3-660.weights", "yolov3-660.cfg") #Not that good
+    net = cv2.dnn.readNet("yolov3.weights", "yolov3.cfg")
+    classes = []
+    with open("coco.names", "r") as f:
+        classes = [line.strip() for line in f.readlines()] 
+    output_layers = [layer_name for layer_name in net.getUnconnectedOutLayersNames()]
+    colors = np.random.uniform(0, 255, size=(len(classes), 3))
+    return net, classes, colors, output_layers
+
+def load_image(img_path):
+    # image loading
+    img = cv2.imread(img_path)
+    img = cv2.resize(img, None, fx=0.4, fy=0.4)
+    height, width, channels = img.shape
+    return img, height, width, channels
+
+def detect_objects(img, net, outputLayers):			
+    blob = cv2.dnn.blobFromImage(img, scalefactor=0.00392, size=(320, 320), mean=(0, 0, 0), swapRB=True, crop=False)
+    net.setInput(blob)
+    outputs = net.forward(outputLayers)
+    return blob, outputs
+
+def get_box_dimensions(outputs, height, width):
+    boxes = []
+    confs = []
+    class_ids = []
+    for output in outputs:
+        for detect in output:
+            scores = detect[5:]
+            class_id = np.argmax(scores)
+            conf = scores[class_id]
+            if conf > 0.3:
+                center_x = int(detect[0] * width)
+                center_y = int(detect[1] * height)
+                w = int(detect[2] * width)
+                h = int(detect[3] * height)
+                x = int(center_x - w/2)
+                y = int(center_y - h / 2)
+                boxes.append([x, y, w, h])
+                confs.append(float(conf))
+                class_ids.append(class_id)
+    return boxes, confs, class_ids
+
+def count_people(classes, boxes, confs, class_ids): 
+    person_counter=0
+    indexes = cv2.dnn.NMSBoxes(boxes, confs, 0.5, 0.4)
+    for i in range(len(boxes)):
+        if i in indexes:
+            label = str(classes[class_ids[i]])
+            if label == 'person':
+                person_counter+=1
+    return person_counter
+
+def draw_labels(boxes, confs, colors, class_ids, classes, img): 
+    person_counter=0
+    indexes = cv2.dnn.NMSBoxes(boxes, confs, 0.5, 0.4)
+    font = cv2.FONT_HERSHEY_PLAIN
+    for i in range(len(boxes)):
+        if i in indexes:
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            if label == 'person':
+                person_counter+=1
+            color = colors[i]
+            cv2.rectangle(img, (x,y), (x+w, y+h), color, 2)
+            cv2.putText(img, label, (x, y - 5), font, 1, color, 1)
+    cv2.imshow("Image", img)
+    print(f'No of persons in the image = {person_counter}')
+
+def image_detect(img_path): 
+    model, classes, colors, output_layers = load_yolo()
+    image, height, width, channels = load_image(img_path)
+    blob, outputs = detect_objects(image, model, output_layers)
+    boxes, confs, class_ids = get_box_dimensions(outputs, height, width)
+    # draw_labels(boxes, confs, colors, class_ids, classes, image)
+    peoples = count_people(classes, boxes, confs, class_ids)
+    print(f'No of persons in the image = {peoples}')
+    # while True:
+    #     key = cv2.waitKey(1)
+    #     if key == 27:
+    #         break
+
+if __name__ == '__main__':
+    image_detect('images/busy_street.jpg')
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Object Detection using Yolo and OpenCV
+
+### <b>Commands to execute the code:</b>
+
+If you want to perform object detection in a static image file,
+```
+python yolo.py --image=True --image_path="folder/imagename.jpg"
+```
+
+If you want to perform object detection in a video,
+```
+python yolo.py --play_video=True --video_path='folder/video.mp4"
+```
+
+If you want to start the webcam and perform real-time object detection,
+```
+python yolo.py --webcam=True
+```
